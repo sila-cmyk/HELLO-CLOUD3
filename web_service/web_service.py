94b3ef5ac2e83dc51ab00ad84d1e852814591e0c
sila-cmyk <slazorlu224@gmail.com>
2026-01-04 18:32:17 +0300

    Update web_service.py

--- web_service/web_service.py
+++ web_service/web_service.py
@@ -10,7 +10,7 @@ HTML = """
 <!doctype html>
 <html>
 <head>
-    <title>Naz Aydemir</title>
+    <title>Sıla Zorlu</title>
     <style>
         body { font-family: Arial; text-align: center; padding: 50px; background: #eef2f3; }
         h1 { color: #333; }
